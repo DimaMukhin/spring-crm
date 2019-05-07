a7b760e7d804e13100af8afa546aee317e79a7b4
DimaMukhin <dima9348@gmail.com>
2019-05-07 10:17:39 -0500

    implemented customer hibernate dao

---
+++ src/springdemo/dao/HibernateCustomerDAO.java
@@ -0,0 +1,32 @@
+package springdemo.dao;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import springdemo.entity.Customer;
+
+@Repository
+public class HibernateCustomerDAO implements CustomerDAO {
+
+	@Autowired
+	private SessionFactory sessionFactory;
+	
+	@Override
+	@Transactional
+	public List<Customer> getCustomers() {
+		Session session = sessionFactory.getCurrentSession();
+		
+		Query<Customer> customersQuesry = session.createQuery("from Customer", Customer.class);
+		
+		List<Customer> customers = customersQuesry.getResultList();
+		
+		return customers;
+	}
+
+}
